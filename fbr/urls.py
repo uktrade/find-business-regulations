09dafcc5345d1b8228e8cc89568b5ffde51eb3ba
Haresh Kainth <haresh.kainth@businessandtrade.gov.uk>
2025-01-13 16:59:30 +0000

    temp:Add test-sentry endpoint for logging integration tests [will be removed in next commit]
    Introduce a test-sentry endpoint in URLs and core views to send test log messages with different levels (INFO, ERROR, WARNING, DEBUG). This is useful for verifying Sentry logging configuration and integration functionality.

--- fbr/urls.py
+++ fbr/urls.py
@@ -134,6 +134,7 @@ urlpatterns = [
         name="hide-cookie-banner",
     ),
     # path("search/", orp_views.search, name="search"),
+    path("test-sentry/", core_views.test_sentry, name="test-sentry"),
 ]
 
 if settings.DJANGO_ADMIN:
